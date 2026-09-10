31e45a1050ceb6794b050f0566b5acd9b06478e5
Luis Vazquez <luis.vazquez@workout.loc>
2026-09-10 20:58:43 +0200

    fix: pulir tarjetas de nota de carga

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import QRCode from "qrcode";
 import JsBarcode from "jsbarcode";
 import BarcodeScanner from "./components/BarcodeScanner";
 
-const APP_VERSION = "2.0.85";
+const APP_VERSION = "2.0.86";
 const APP_ENVIRONMENT = process.env.NODE_ENV === "production" ? "Producción" : "Local";
 
 const initialModules = [
